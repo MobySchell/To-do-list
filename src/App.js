79a2995ee9e31b430579d7202b6c011dfa5c49b3
Joshua Schell <jagschell2000@gmail.com>
2021-07-15 07:25:49 +0200

    Firebase Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React, { Component } from "react";
 
+import firebase from "./Firebase/firebase";
+import Book from "./models/Book";
+
 import BookInput from "./components/BookInput";
 import BookTable from "./components/BookTable";
 
@@ -8,26 +11,53 @@ export default class App extends Component {
     constructor(props) {
         super(props);
 
-        const booksString = localStorage.getItem("books");
+        this.db = firebase.firestore();
 
-        const books = JSON.parse(booksString ? booksString : "[]")
-
-        this.state = {books: books};
+        this.state = { books: [] };
     }
 
-    updateBooks(books) {
-        this.setState({books: books})
-        localStorage.setItem('books', JSON.stringify(books))
+    componentDidMount() {
+        this.fetchData();
     }
 
-    onBookSubmitted(book) {
-        this.state.books.push(book);
-        this.updateBooks(this.state.books);
+    async fetchData() {
+        try {
+            const snapshot = await this.db.collection("books").get();
+            const books = snapshot.docs.map((doc) => Book.fromFB(doc));
+
+            this.setState({ books: books });
+        } catch (err) {
+            console.log(err);
+        }
     }
 
-    onBookRemoved(bookId) {
-        const updatedBookArr = this.state.books.filter((book) => book.id !== bookId)
-        this.updateBooks(updatedBookArr)
+    async onBookSubmitted(book) {
+        try {
+            const dbAdd = this.db.collection("books").doc();
+            await dbAdd.set({
+                title: book.title,
+                author: book.author,
+                isbn: book.isbn,
+            });
+
+            book.id = dbAdd.id;
+            this.state.books.push(book);
+            this.setState({ books: this.state.books });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async onBookRemoved(bookId) {
+        try {
+            await this.db.collection("books").doc(bookId).delete();
+            const updatedBookArr = this.state.books.filter(
+                (book) => book.id !== bookId
+            );
+            this.setState({ books: updatedBookArr });
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     render() {
@@ -39,8 +69,10 @@ export default class App extends Component {
                         <BookInput
                             createBook={(book) => this.onBookSubmitted(book)}
                         />
-                        <BookTable books={this.state.books} 
-                        bookRemoved={(bookId) => this.onBookRemoved(bookId)} />
+                        <BookTable
+                            books={this.state.books}
+                            bookRemoved={(bookId) => this.onBookRemoved(bookId)}
+                        />
                     </div>
                 </div>
             </>
---
+++ src/Firebase/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDFQEOt6nWKNWdG-cOh5OZwIZd5nl9jzOk",
+    authDomain: "booklist-schell.firebaseapp.com",
+    projectId: "booklist-schell",
+    storageBucket: "booklist-schell.appspot.com",
+    messagingSenderId: "1084193017889",
+    appId: "1:1084193017889:web:c6399df53c288d8d641360",
+    measurementId: "G-XWVY8RD8LF",
+});
+
+export default firebase;
