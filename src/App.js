ea6bec6e5ca042d3194109f625d16842bf6c8551
Joshua Schell <jagschell2000@gmail.com>
2021-07-21 21:57:56 +0200

    auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.css"
 
 import { BrowserRouter, Route } from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.css";
+
+import Navbar from "./components/Navbar";
 import Home from "./login/Home";
 import Login from "./login/Login";
 import Register from "./login/Register";
-
 
 export default class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                
+                <Navbar />
 
-                <Route path="/" exact component={Home}/>
-                <Route path="/" component={Login}/>
-                <Route path="/" component={Register}/>
+                <Route path="/" exact component={Home} />
+                <Route path="/" component={Login} />
+                <Route path="/" component={Register} />
             </BrowserRouter>
-        )
+        );
     }
 }
